1d1ab401c0798c100e9031295cbe6adfc5419cad
T8671076 <asaf.haas.40@gmail.com>
2020-03-25 23:45:15 +0200

    Configuration Changes

---
+++ MevudadiM/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from .commands import create_tables
+from .extensions import db, login_manager
+from .models import User
+
+def create_app(config_file='settings.py'):
+    app = Flask(__name__)
+
+    app.config.from_pyfile(config_file)
+
+    db.init_app(app)
+
+    app.cli.add_command(create_tables)
+
+    return app
--- MevudadiM/app.py
+++ MevudadiM/app.py
@@ -1,14 +1,65 @@
-from flask import Flask
-from .extensions import db
-from .models import User, Room
+from flask import Flask, request, render_template, make_response
+
+import MevudadiM.zoom_user as zoom_user
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 
+users = []
 
-@app.route('/update_floor', methods=["GET"])
-def update_floor():
+@app.route('/', methods=["GET"])
+def homepage():
+    code = request.args.get("code")
 
-    return
+    # check if this is a new user in the site, and if so add to the db of users
+    username = "<Not logged in>"
+    if code is not None:
+        new_user = zoom_user.User(code=code)
+        username = "%s %s" % (
+            new_user.account_info["first_name"],
+            new_user.account_info["last_name"],
+        )
+        users.append((username, new_user))
+
+    resp = make_response(render_template(
+        'index.html',
+        greeting="Welcome %s" % username,
+        title="מחזור מ האגדי - דף הבית"
+    ))
+    resp.set_cookie("username", username)
+    return resp
+
+
+@app.route('/enter_room_first', methods=["POST", "GET"])
+def enter_room():
+    # TODO: Get room argument and store in database
+
+    username = request.cookies.get("username")
+
+    # get user object from list / db
+    user_obj = [user for name, user in users if name == username][0]
+    print("GOT USER %s" % str(user_obj))
+
+    meeting = user_obj.create_meeting()
+
+    return str(meeting)
+
+
+@app.route('/participant_joined', methods=["POST"])
+def participant_joined():
+    pass
+
+
+@app.route('/participant_left', methods=["POST"])
+def participant_joined():
+    pass
+
+
+@app.route('/meeting_ended', methods=["POST"])
+def participant_joined():
+    pass
+
+
+
 
 if __name__ == '__main__':
     app.run()
