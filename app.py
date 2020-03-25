68cdae59e43545223acef836d2f35d2bf1acd96a
Royz2123 <1Qazwsxdc>
2020-03-25 23:27:12 +0200

    Debugging

--- app.py
+++ app.py
@@ -1,28 +1,65 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 
 import zoom_user
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 
+users = []
 
 @app.route('/', methods=["GET"])
-def hello():
+def homepage():
     code = request.args.get("code")
 
     # check if this is a new user in the site, and if so add to the db of users
-    content = "Not logged in"
+    username = "<Not logged in>"
     if code is not None:
         new_user = zoom_user.User(code=code)
-        content = "Welcome %s %s" % (
+        username = "%s %s" % (
             new_user.account_info["first_name"],
             new_user.account_info["last_name"],
         )
+        users.append((username, new_user))
 
-    print(code)
-    return render_template(
+    resp = make_response(render_template(
         'index.html',
-        greeting=content,
-        title="Jinja Demo Site")
+        greeting="Welcome %s" % username,
+        title="מחזור מ האגדי - דף הבית"
+    ))
+    resp.set_cookie("username", username)
+    return resp
+
+
+@app.route('/enter_room_first', methods=["POST", "GET"])
+def enter_room():
+    # TODO: Get room argument and store in database
+
+    username = request.cookies.get("username")
+
+    # get user object from list / db
+    user_obj = [user for name, user in users if name == username][0]
+    print("GOT USER %s" % str(user_obj))
+
+    meeting = user_obj.create_meeting()
+
+    return str(meeting)
+
+
+@app.route('/participant_joined', methods=["POST"])
+def participant_joined():
+    pass
+
+
+@app.route('/participant_left', methods=["POST"])
+def participant_joined():
+    pass
+
+
+@app.route('/meeting_ended', methods=["POST"])
+def participant_joined():
+    pass
+
+
+
 
 if __name__ == '__main__':
     app.run()
