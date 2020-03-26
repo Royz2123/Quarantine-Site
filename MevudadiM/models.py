f0c5497b93fcd9ee9b83a2c2eb23dcaf2930173d
T8671076 <asaf.haas.40@gmail.com>
2020-03-26 02:12:28 +0200

    Added commands

--- MevudadiM/models.py
+++ MevudadiM/models.py
@@ -1,16 +1,18 @@
 from .extensions import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(200))
     access_token = db.Column(db.Text)
     refresh_token = db.Column(db.Text)
 
 
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    room_id = db.Column(db.Integer)
-    room_name = db.Column(db.String)
+    room_name = db.Column(db.Integer)
+    meeting_name = db.Column(db.String(200))
     floor = db.Column(db.Integer)
-    meeting_id = db.Column(db.String(20))
+    join_url = db.Column(db.String(200))
+    meeting_id = db.Column(db.String(200))
     participants = db.Column(db.Text)
